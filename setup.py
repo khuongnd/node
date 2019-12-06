6c94a3b0899484bfdec868d443f53fc19d3a4166
duykhuong.nguyen <Youvo@2009>
2019-12-06 11:51:33 +0900

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='node',
+      version='1.0',
+      description='A supplementary code for Neural Oblivious Decision Ensembles for Deep Learning on Tabular Data paper',
+      url='https://github.com/Qwicen/node',
+      author='Sergei Popov, Stanislav Morozov, Artem Babenko',
+      author_email='',
+      license='',
+      packages=['node'],
+      zip_safe=False
+)
